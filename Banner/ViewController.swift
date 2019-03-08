24059aa1dff9c8024e80500c0c6bec29f51f374c
Yi Tong <yitong@hieus-imac.local>
2019-03-08 11:21:30 -0800

    Initial Commit

---
+++ Banner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Banner
+//
+//  Created by Yi Tong on 3/8/19.
+//  Copyright © 2019 Yi Tong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
